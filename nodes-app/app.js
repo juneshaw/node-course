3ed6ab2a4150aa4df5e0b1e51799f446715f15ee
Shaw <june.shaw@sling.com>
2019-06-12 10:14:14 -0600

    Practice with yargs parser

--- nodes-app/app.js
+++ nodes-app/app.js
@@ -1,6 +1,48 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-console.log(getNotes())
-console.log(chalk.green.bgWhite.bold('Success'))
-console.log(chalk`{green Success with template}`)
+yargs.version('1.1.0')
+// console.log('args:', yargs)
+// console.log('process argv: ', process.argv)
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Adding a new note', argv.title)
+    }
+})
+
+yargs.command({
+    command: 'delete',
+    describe: 'Delete a note',
+    handler: function() {
+        console.log('Deleting a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('List notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Read a note')
+    }
+})
+
+yargs.parse()
