8ed06474dff90b8baf6d2d3b6a123cb15561b168
Shaw <june.shaw@sling.com>
2019-06-12 08:18:15 -0600

    NPM module usage

--- nodes-app/app.js
+++ nodes-app/app.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 console.log(getNotes())
+console.log(chalk.green.bgWhite.bold('Success'))
+console.log(chalk`{green Success with template}`)
