9afb1f34549ac36d8d830fb7ea72d58f09bce6e1
Shaw <june.shaw@sling.com>
2019-06-13 19:33:29 -0600

    Customizing data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/45811978579347844fcf844063d5e3ba/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/45811978579347844fcf844063d5e3ba/39.63,105.31?units=us'
 
 request.get({url: url, json: true, strictSSL: false}, (error, response) => {
-    console.log(`It is currently ${response.body.currently.temperature} degrees out.  There is a ${response.body.currently.precipProbability} chance of rain.`)
+    console.log(`${response.body.daily.data[0].summary}  It is currently ${response.body.currently.temperature} degrees out.  There is a ${response.body.currently.precipProbability}% chance of rain.`)
 })
